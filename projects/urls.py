8f4fc0b9309202005c662fc285e2424e8a835050
shalu john <shalu@yougotagift.com>
2023-08-30 12:44:48 +0530

    updated projects/urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -3,10 +3,14 @@ from.import views
 
 
 urlpatterns = [
-path('', views.projects, name="projects"),
-path('project/<str:pk>/', views.project, name="project"),
-path('create=project/', views.createProject,name="create-Project"),
-path('update-project/<str:pk>/', views.updateProject,name="update-project"),
-path('delete-project/<str:pk>',views.deleteProject,name="delete-project"),
+    path('', views.projects, name="projects"),
+    path('project/<str:pk>/', views.project, name="project"),
+    path('create=project/', views.createProject,name="create-Project"),
+    path('update-project/<str:pk>/', views.updateProject,
+         name="update-project"),
+    path('delete-project/<str:pk>',views.deleteProject,name="delete-project"),
+    path('login/', views.loginUser, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name="register"),
     ]
 
